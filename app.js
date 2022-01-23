2109b59ba276b1686fb3f3820ba55e7f3b6d6b61
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-23 17:14:13 -0300

    PUERTO 3030 correccion

--- app.js
+++ app.js
@@ -2,14 +2,10 @@ const express = require('express')
 const res = require('express/lib/response')
 const app = express()
 
-app.listen(3100, () => {
-  console.log('Levante WEB SERVER en port 3000')
+app.listen(3030, () => {
+  console.log('Levante WEB SERVER en port 3030')
 })
 
 
 //CANNOT GET / EN localhost:3000 porque no definimos resposta en rua raiz
 // COMMAND C FRENA EJECUCION 
-
-app.get('/' , function(req, res){
-  res.send('INGRESE A Mercado LIEBRE')
-})
