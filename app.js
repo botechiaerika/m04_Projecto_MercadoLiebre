add479f38816765698886e840cb032fe1d764096
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-23 21:38:48 -0300

    creando web PAGE mercado LIEBRE

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.listen(3030, () => {
+app.listen(3030, () =>{
   console.log('Levante WEB SERVER en port 3030')
 })
 
+/*app.get('/home', (req, res) =>{
+  res.sendFile((__dirName + 'views/home.html'));
+})*/
 
-//CANNOT GET / EN localhost:3000 porque no definimos resposta en rua raiz
-// COMMAND C FRENA EJECUCION 
+app.listen(3030, () => {
+  console.log('Levante WEB SERVER en port 3030')
+})
+//CANNOT GET EN localhost:3000 porque no definimos resposta en rua raiz
+//COMMAND C FRENA EJECUCION 
