de748fd241c2e5478ad4ae2f29120f773276bcfd
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-25 20:12:59 -0300

    commit 01

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-// WEB SERVER - SEND.FILE METHOD
-
-const express = require('express')
-const app = express();
-=======
-//WEB SERVER - SEND.FILE METHOD
 const express = require("express")
 const app = express()
 const path = require("path")
@@ -12,26 +5,14 @@ app.use(express.static(path.join(__dirname + './public')))
 
 
 
-express.static(path.join(__dirname, './views/home.html') res.app.get('/Home', (req, res) => {
-        sendFile(pathHome)
-    });
+let pathHome = path.join(__dirname, './views/home.html');
+app.get('/Home', (req, res) => {
+    sendFile(path.resolve(pathHome))
+});
 
-    function(req, res, next) {
-        res.render('home.ejs');
-    })
->>>>>>> 289585e90b34005c140bc624d160f339dc9b7596
+let pathPublic = path.join(__dirname, './public');
+app.use(express.static(path.resolve(pathPublic)));
 
 app.listen(3030, () => {
     console.log('LEVANTE RUTA PORT 3030')
 });
-
-// LEVANTAR HTML EN WEB SERVER:
-
-const path = require('path');
-const pathPublic = path.resolve(__dirname, '. / public');
-app.use(express.static('public'));
-
-app.get('/home', (req, res) => {
-    let pathHome = path.resolve(__dirname, './views/home.html')
-    res.sendFile(pathHome)
-})
