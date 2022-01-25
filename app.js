401c932499fbecbbb591e8e99fae7eb74f87323a
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-25 12:46:47 -0300

    web server ok con html + css

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-//WEB SERVER - SEND.FILE METHOD
-const express = require('express')
-const app = express()
-const path = require('path')
-
-app.get('/Home', (req, res) => {
-    let pathHome = path.resolve(__dirname, './views/home.html')
-    res.sendFile(pathHome)
-});
+// WEB SERVER - SEND.FILE METHOD
+const express = require('express');
+const app = express();
 
 app.listen(3030, () => {
     console.log('LEVANTE RUTA PORT 3030')
 });
+
+// LEVANTAR HTML EN WEB SERVER:
+
+const path = require('path');
+const pathPublic = path.resolve(__dirname, '. / public');
+app.use(express.static('public'));
+
+app.get('/home', (req, res) => {
+    let pathHome = path.resolve(__dirname, './views/home.html')
+    res.sendFile(pathHome)
+})
