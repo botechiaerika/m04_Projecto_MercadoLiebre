a50b5674637344b83c3eb2789dfd538a0efbb0ba
Erika Botechia <mendoncabotechiaerikaluisa@gmail.com>
2022-01-25 19:05:37 -0300

    merge

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const path = require("path")
 app.use(express.static(path.join(__dirname + './public')))
 
+<<<<<<< Updated upstream
 
 
 express.static(path.join(__dirname, './views/home.html') res.app.get('/Home', (req, res) => {
@@ -14,6 +15,13 @@ express.static(path.join(__dirname, './views/home.html') res.app.get('/Home', (r
         res.render('home.ejs');
     })
 
+=======
+app.get('/Home', (req, res) => {
+    let pathHome = './public'.resolve(__dirname, './views/home.html')
+    res.sendFile(pathHome)
+});
+app.use(express.static(path.join(__dirname, './public')));
+>>>>>>> Stashed changes
 app.listen(3030, () => {
     console.log('LEVANTE RUTA PORT 3030')
 });
