7861ff9180219babe0aeee0205903a694e965f9d
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-26 00:02:05 -0300

    version

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 //WEB SERVER - SEND.FILE METHOD
 const express = require("express")
+const express = require('express ')
 const app = express()
 const path = require("path")
+const path = require('path')
 const pathPublic = path.join(__dirname + './public');
 
 let pathHome = './public'.resolve(__dirname, './views/home.html')
 app.use(express.static(publicPath) a pp.get('/Home', (req, res) => res.sendFile(pathHome)
 })
 
-});
+});app.get('/views/home.html', (req res) => {
+            res.sendFile(path.resolve(pathHome)
+            }) app.listen(3030, () => {
+                console.log('LEVANTE RUTA PORT 3030')
